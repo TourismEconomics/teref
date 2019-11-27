3717f4e6a3671117e92a7d25dccd73a866977b3b
aranryan <aran.ryan@gmail.com>
2019-11-27 14:59:04 -0500

    rework cb_ctry_oegtc

--- data-raw/cb_ctry_oegtc.R
+++ data-raw/cb_ctry_oegtc.R
@@ -2,6 +2,6 @@ library(tidyverse)
 library(usethis)
 
 
-cb_ctry_oegtc <- read_csv(file="data-raw/cb_ctry_oegtc.csv", col_names = TRUE, col_types = "cccccc")
+cb_ctry_oegtc <- read_csv(file="data-raw/cb_ctry_oegtc.csv", col_names = TRUE, col_types = "ccccccc")
 
 usethis::use_data(cb_ctry_oegtc, internal = FALSE)
