284c5403cbf4197ae8d8ee4a0d515119bb47436b
aranryan <aran.ryan@gmail.com>
2019-12-10 21:44:28 -0500

    add tld code description

--- R/data.R
+++ R/data.R
@@ -18,6 +18,9 @@
 #' \item{oegts_region_midlev}{Oxford Economics Global Travel Service mid-level region}
 #' \item{oegts_region_highlev}{Oxford Economics Global Travel Service high-level region. These include: Europe, Africa and Middle East, Americas, Asia Pacific, and NA.}
 #' \item{oegts_origin_code}{Oxford Economics Global Travel Service origin code}
+#' \item{internet_tld}{Country code top-level domain. Most tld codes are identical to ISO 3166-1
+#' codes, with some notable exceptions. For further details,
+#' see \url{https://en.wikipedia.org/wiki/Country_code_top-level_domain}}
 #' \item{note}{Note}}
 "cb_ctry"
 
