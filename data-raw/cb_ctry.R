09f498bb2338ec28bd5f775f4ce0230566d3f2ae
aranryan <aran.ryan@gmail.com>
2019-12-10 21:38:12 -0500

    add internet codes

--- data-raw/cb_ctry.R
+++ data-raw/cb_ctry.R
@@ -2,6 +2,6 @@ library(dplyr)
 library(readr)
 library(usethis)
 
-cb_ctry <- read_csv(file="data-raw/cb_ctry.csv", col_names = TRUE, col_types = "cccccccccccccc")
+cb_ctry <- read_csv(file="data-raw/cb_ctry.csv", col_names = TRUE, col_types = "ccccccccccccccc")
 
 use_data(cb_ctry, internal = FALSE)
