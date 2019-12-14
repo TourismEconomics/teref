dfdd07ddfb7e8f0800a6bc9788c02f6c6a86d597
aranryan <aran.ryan@gmail.com>
2019-12-14 18:31:40 -0500

    add geo friendly market names to some that were missing

--- data-raw/cb_city.R
+++ data-raw/cb_city.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 library(usethis)
+#library(here)
 
 
 cb_city <- read_csv(file="data-raw/cb_city.csv", col_names = TRUE, col_types = "ccccccccccc")
+
+# cb_city <- cb_city %>%
+#   mutate(mkt_geocode_friendly = ifelse(is.na(mkt_geocode_friendly), mkt_name,
+#                                        mkt_geocode_friendly))
+# write_csv(cb_city, "cb_city.csv")
 
 usethis::use_data(cb_city, internal = FALSE)
