ce231aedbc6b47108d911018f0cda13d10cd0c49
aranryan <aran.ryan@gmail.com>
2019-12-15 00:37:36 -0500

    redo cb_city to match latest global cities names and codes

--- data-raw/cb_city.R
+++ data-raw/cb_city.R
@@ -3,11 +3,27 @@ library(usethis)
 #library(here)
 
 
-cb_city <- read_csv(file="data-raw/cb_city.csv", col_names = TRUE, col_types = "ccccccccccc")
+cb_city <- read_csv(file="data-raw/cb_city.csv", col_names = TRUE, col_types = "ccccccccc")
+
+ # cb_city <- cb_city %>%
+ #  mutate(mkt_geocode_friendly = mkt_name)
+
+a <- cb_city %>%
+  group_by(mkt_name) %>%
+  filter(n()>1)
+stopifnot(nrow(a)==0)
+
+# cb_ctry_a <- cb_ctry %>%
+#   select(country_name, iso3166_1a3)
+#
+# cb_city <- cb_city %>%
+#   left_join(cb_ctry_a, by=c("country_name"))
 
 # cb_city <- cb_city %>%
-#   mutate(mkt_geocode_friendly = ifelse(is.na(mkt_geocode_friendly), mkt_name,
-#                                        mkt_geocode_friendly))
-# write_csv(cb_city, "cb_city.csv")
+#   select(mkt_name, mkt_sh, country_name, iso3166_1a3,
+#          oegc_name, oegc_loc_code, oegc_ctry_name, mkt_geocode_friendly, oe_gct_name,
+#          oe_gct_location_code, note)
+
+# write_excel_csv(cb_city, "cb_city.csv")
 
 usethis::use_data(cb_city, internal = FALSE)
