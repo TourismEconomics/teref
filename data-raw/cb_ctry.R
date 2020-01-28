16bb454cf5217d38c8e4ebe00dcb36b19f58cbe6
aranryan <aran.ryan@gmail.com>
2020-01-28 11:00:44 -0500

    add several small countries

--- data-raw/cb_ctry.R
+++ data-raw/cb_ctry.R
@@ -4,4 +4,20 @@ library(usethis)
 
 cb_ctry <- read_csv(file="data-raw/cb_ctry.csv", col_names = TRUE, col_types = "ccccccccccccccc")
 
+
+# check for duplicates
+
+stopifnot(!any(duplicated(cb_ctry$country_name)))
+stopifnot(!any(duplicated(cb_ctry$iso3166_1a3)))
+
+# check for duplicates, handling na
+temp <- cb_ctry %>%
+  filter(!(is.na(internet_tld))) %>%
+  group_by(internet_tld) %>%
+  filter(n()>1)
+stopifnot(nrow(temp)==0)
+
+
+# use table
+
 use_data(cb_ctry, internal = FALSE)
